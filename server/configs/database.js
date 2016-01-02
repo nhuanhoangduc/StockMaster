923f77366ffd36cd6466de03ccac298a5fb2f0c3
nhuanhoangduc <nhuan.hoangduc@outlook.com>
2016-01-02 14:44:46 +0700

    config db

--- server/configs/database.js
+++ server/configs/database.js
@@ -1,9 +1,9 @@
 var mongoose = require('mongoose');
 
 /* connect local database */
-mongoose.connect('mongodb://localhost:27017/stockDB');
+//mongoose.connect('mongodb://localhost:27017/stockDB');
 
 /* connect database hosting */
-//mongoose.connect('mongodb://nhuanhoangduc:vansay@ds037145.mongolab.com:37145/nhuandb');
+mongoose.connect('mongodb://nhuanhoangduc:vansay@ds037145.mongolab.com:37145/nhuandb');
 
 module.exports = mongoose;
